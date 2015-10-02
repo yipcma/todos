7aa1cfd5b2fabac9e9c0b429a4cde60192c86af7
Andrew Yip <yipcma@gmail.com>
2015-10-02 09:10:52 +0300

    Managing Todos: keyboard events

--- todos.js
+++ todos.js
@@ -10,9 +10,9 @@ if (Meteor.isClient) {
       });
     }
   });
-  
+
   Template.todos.events({
-    'submit form': function(event){
+    'submit form': function(event) {
       event.preventDefault();
       var todoName = $('[name="todoName"]').val(); // note the use of jQuery here
       Todos.insert({
@@ -23,21 +23,33 @@ if (Meteor.isClient) {
       $('[name="todoName"]').val('');
     }
   });
-  
+
   Template.todoItem.events({
-    'click .delete-todo': function(event){
+    'click .delete-todo': function(event) {
       event.preventDefault();
       var documentId = this._id;
       var confirm = window.confirm("Delete this task?");
-      if(confirm){
-        Todos.remove({_id: documentId});
+      if (confirm) {
+        Todos.remove({
+          _id: documentId
+        });
       }
     },
-    'keyup [name=todoItem]': function(event){
-      var documentId = this._id;
-      var todoItem = $(event.target).val();
-      Todos.update({_id: documentId}, {$set: {name: todoItem}});
-      console.log(documentId, " changed to ",  todoItem);
+    'keyup [name=todoItem]': function(event) {
+      if (event.which == 13 || event.which == 27) {
+        $(event.target).blur();
+      }
+      else {
+        var documentId = this._id;
+        var todoItem = $(event.target).val();
+        Todos.update({
+          _id: documentId
+        }, {
+          $set: {
+            name: todoItem
+          }
+        });
+      }
     }
   });
 }
