d91fe6e1414a4ad018cbfb8908918f893a38d073
Andrew Yip <yipcma@gmail.com>
2015-10-08 12:15:14 +0300

    user accounts: error handling part 1

--- todos.js
+++ todos.js
@@ -184,7 +184,14 @@ if (Meteor.isClient) {
       event.preventDefault();
       var email = $('[name=email]').val();
       var password = $('[name=password]').val();
-      Meteor.loginWithPassword(email, password);
+      Meteor.loginWithPassword(email, password, function(error) {
+        if (error) {
+          console.log(error.reason);
+        }
+        else {
+          Router.go("home");
+        }
+      });
     }
   });
 }
