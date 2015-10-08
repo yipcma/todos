13fe7028fc327a36d2010cf4fa57d181ab276f95
Andrew Yip <yipcma@gmail.com>
2015-10-08 10:33:13 +0300

    routing, part 2: retrieving data

--- todos.js
+++ todos.js
@@ -15,7 +15,10 @@ Router.route('/register');
 Router.route('/login');
 
 Router.route('/list/:_id', {
+  template: 'listPage',
   data: function(){
+    var currentList = this.params._id;
+    return Lists.findOne({_id: currentList});
   }
 });
 
