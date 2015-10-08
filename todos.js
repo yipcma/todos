6aba45e9056f1056db8e9898306a8bebcb6f7267
Andrew Yip <yipcma@gmail.com>
2015-10-08 10:56:18 +0300

    routing, part 2: counting tasks

--- todos.js
+++ todos.js
@@ -118,10 +118,15 @@ if (Meteor.isClient) {
 
   Template.todosCount.helpers({
     'totalTodos': function() {
-      return Todos.find().count();
+      var currentList = this._id;
+      return Todos.find({
+        listId: currentList
+      }).count();
     },
     'completedTodos': function() {
+      var currentList = this._id;
       return Todos.find({
+        listId: currentList,
         completed: true
       }).count();
     }
