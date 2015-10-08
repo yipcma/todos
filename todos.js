14a4769a6220eed7a666af9686a010493794950d
Andrew Yip <yipcma@gmail.com>
2015-10-08 11:17:49 +0300

    routing, part 2: redirecting routes

--- todos.js
+++ todos.js
@@ -139,6 +139,8 @@ if (Meteor.isClient) {
       var listName = $('[name=listName]').val();
       Lists.insert({
         name: listName
+      }, function(error, results){
+        Router.go('listPage', {_id: results});
       });
       $('[name=listName]').val('');
     }
